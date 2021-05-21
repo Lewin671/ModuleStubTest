d5d565944289214612304691fa3ae486b03b638b
Lewin671 <2596736318@qq.com>
2021-05-22 01:46:56 +0800

    add the app module

---
+++ app/src/androidTest/java/com/example/modulestubtest/ExampleInstrumentedTest.kt
@@ -0,0 +1,28 @@
+package com.example.modulestubtest
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import com.example.lib.coroutineRequest
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.example.modulestubtest", appContext.packageName)
+    }
+
+    @Test
+    fun coroutineTest() {
+        coroutineRequest()
+    }
+}
