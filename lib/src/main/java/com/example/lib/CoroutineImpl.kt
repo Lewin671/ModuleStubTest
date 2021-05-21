4745b2b128dcd59c7c55a01519e1c6a58716d1c0
Lewin671 <2596736318@qq.com>
2021-05-22 01:46:43 +0800

    add the lib module

---
+++ lib/src/main/java/com/example/lib/CoroutineImpl.kt
@@ -0,0 +1,35 @@
+package com.example.lib
+
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import kotlin.system.measureTimeMillis
+
+
+suspend fun request(url: String) {
+    delay(1000)
+
+    println("request $url")
+}
+
+
+fun coroutineRequest() {
+    val urls = listOf("https://baidu.com", "https://qingyingliu.top")
+    runBlocking {
+        val costTime = measureTimeMillis {
+            urls.map {
+                async {
+                    request(it)
+                }
+            }.forEach {
+                it.await()
+            }
+        }
+        println("running time costs $costTime ms")
+    }
+}
+
+// 测试程序
+fun main() {
+    coroutineRequest()
+}
